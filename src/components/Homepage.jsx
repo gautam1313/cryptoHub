754ac5f220eac85098254db2fe40eae64adaf54f
Sebby80 <sebkm80@gmail.com>
2021-12-10 19:22:10 +0530

    Showing cryptos and news in brief in the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,7 +6,7 @@ import { Cryptocurrencies, News } from "../components";
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
   console.log(data);
   const { Title } = Typography;
@@ -20,30 +20,33 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={globalStats?.total}
+          />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Exchanges"
-            value={millify(globalStats.totalExchanges)}
+            value={millify(globalStats?.totalExchanges)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Market Cap"
-            value={millify(globalStats.totalMarketCap)}
+            value={millify(globalStats?.totalMarketCap)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total 24h Volume"
-            value={millify(globalStats.total24hVolume)}
+            value={millify(globalStats?.total24hVolume)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Markets"
-            value={millify(globalStats.totalMarkets)}
+            value={millify(globalStats?.totalMarkets)}
           />
         </Col>
       </Row>
