92ce981542a15b2160d2f465ff7adaacef77f5c8
Sebby80 <sebkm80@gmail.com>
2021-12-11 13:51:07 +0530

    Adding dropdown list to select and filter crypto news in expanded view

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { Typography, Card, Row, Col, Avatar, Select } from "antd";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -10,9 +11,11 @@ const defaultCryptoImg =
   "https://bernardmarr.com/img/What%20is%20the%20Difference%20Between%20Blockchain%20And%20Bitcoin.png";
 
 const News = ({ simplified }) => {
+  const { data } = useGetCryptosQuery(10);
   const count = simplified ? 5 : 10;
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
   const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+    newsCategory,
     count,
   });
   console.log("News ", cryptoNews);
@@ -21,6 +24,28 @@ const News = ({ simplified }) => {
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
+          <Select
+            placeholder="Select Crypto"
+            optionFilterProp="label"
+            className="select-news"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(inputValue, option) =>
+              option.label.toLowerCase().includes(inputValue.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency" key="9148094">
+              Cryptocurrency
+            </Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name} key={coin.id}>
+                {coin.name}
+              </Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews?.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
