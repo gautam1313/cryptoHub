b3f055fdcd89e94595c7660e01b4b769c7a7cd73
Sebby80 <sebkm80@gmail.com>
2021-12-14 19:21:56 +0530

    Adding middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
 import { cryptoNewsApi } from "../services/cryptoNewsApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => (
+    getDefaultMiddleware().concat(cryptoApi.middleware),
+    getDefaultMiddleware().concat(cryptoNewsApi.middleware)
+  ),
 });
+
+// setupListeners(configureStore.dispatch);
